6bf12ceb266044ace0628a322a9402c8c71b29bd
Oh...Yeah!!! <614988210@qq.com>
2024-03-25 12:47:33 +0800

    测试vuex仓库的严格模式

--- src/components/Son1.vue
+++ src/components/Son1.vue
@@ -3,13 +3,19 @@
       <h2>Son1 子组件</h2>
       从vuex中获取的值: <label></label>
       <br>
-      <button>值 + 1</button>
+      <button @click="handleAdd">值 + 1</button>
     </div>
   </template>
 
 <script>
 export default {
-  name: 'Son1Com'
+  name: 'Son1Com',
+  methods: {
+    handleAdd () {
+      this.$store.state.count++
+      console.log(this.$store.state.count)
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  strict: true,
   state: {
     title: '大标题',
     count: 100
